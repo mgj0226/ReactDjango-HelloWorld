3f6c728503bc3b805a851d1b4bdaec8725a03aa8
Michael <michael27mic@gmail.com>
2025-06-19 21:18:23 +0100

    update

--- helloDjReact/helloProject/reactapp/src/App.js
+++ helloDjReact/helloProject/reactapp/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello World</p>
+        <a href='mgj0226.github.io/ReactDjango-HelloWorld'><b><p>To Django Admin Page</p></b></a>
+        <p>*Please use guest as username and G12345678 as password.</p>
       </header>
     </div>
   );
